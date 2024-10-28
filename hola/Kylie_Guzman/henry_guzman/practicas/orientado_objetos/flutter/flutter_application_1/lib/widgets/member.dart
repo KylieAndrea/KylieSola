1e852ce3880c28450d490d746d6809728fca8c27
KylieAndrea <kylieguzmanh@gmail.com>
2024-10-28 03:10:57 -0500

    .

--- hola/Kylie_Guzman/henry_guzman/practicas/orientado_objetos/flutter/flutter_application_1/lib/widgets/member.dart
+++ hola/Kylie_Guzman/henry_guzman/practicas/orientado_objetos/flutter/flutter_application_1/lib/widgets/member.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(User());
+  runApp(Member());
 }
 
-class User extends StatelessWidget {
+class Member extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
