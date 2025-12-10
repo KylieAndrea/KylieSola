72f03016a19bd9b07c96964a9af9049c56b90ab4
Harrinsson <kylieguzmanh@gmail.com>
2025-12-10 02:04:15 -0500

    .

--- hola/Kylie_Guzman/henry_guzman/practicas/orientado_objetos/HTML & CSS/prueba/script.js
+++ hola/Kylie_Guzman/henry_guzman/practicas/orientado_objetos/HTML & CSS/prueba/script.js
@@ -1,16 +1,18 @@
-function Buscar(){
+function funcion(){
     let id = document.getElementById("input").value;
-
     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
       .then(response => response.json())
       .then(json => {
-        var a = document.querySelector("body > table > tbody > tr:nth-child(1) > td:nth-child(2)")
-        a.innerHTML = json.id
 
-        var b = document.querySelector("body > table > tbody > tr:nth-child(2) > td:nth-child(2)")
-        b.innerHTML = json.title
+        var a1 = document.querySelector("body > table > tbody > tr:nth-child(1) > td:nth-child(2)")
+        a1.innerHTML = json.id
 
-        var c = document.querySelector("body > table > tbody > tr:nth-child(3) > td:nth-child(2)")
-        c.innerHTML = json.completed
+        var a2 = document.querySelector("body > table > tbody > tr:nth-child(2) > td:nth-child(2)")
+        a2.innerHTML = json.title
+
+        var a3 = document.querySelector("body > table > tbody > tr:nth-child(3) > td:nth-child(2)")
+        a3.innerHTML = json.completed
+
       })
+
 }
